f3b327a2a22cc53915111282a86db0803cd7b5ad
Hürol Aslan <freebelion@gmail.com>
2022-03-11 18:30:45 +0300

    Çözüm grubuna sanal klasörler ile yeni proje eklendi.

---
+++ HesapProje1/HesapForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapProje1
+{
+    public partial class HesapForm : Form
+    {
+        public HesapForm()
+        {
+            InitializeComponent();
+
+            FormOlustur();
+        }
+
+        private void FormOlustur()
+        {
+            this.Text = "Hesap Makinesi";
+            this.BackColor = Color.AliceBlue;
+            // this.Size = new System.Drawing.Size(400, 500);
+            this.Width = 400;
+            this.Height = 500;
+        }
+    }
+}
