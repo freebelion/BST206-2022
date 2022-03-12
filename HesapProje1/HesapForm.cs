3afd97fbdddbeed7512c7e3817cf242c3bcc8d99
Hürol Aslan <freebelion@gmail.com>
2022-03-12 14:52:33 +0300

    HesapProje1 her seferinde tek bir işlem yapacak hale getirildi.

--- HesapProje1/HesapForm.cs
+++ HesapProje1/HesapForm.cs
@@ -13,7 +13,7 @@ namespace HesapProje1
     public partial class HesapForm : Form
     {
         /// <summary>
-        /// Kontrolleri temsil edecek değişken tanımlarını bu bölgede topladık.
+        /// Form sınıfına ait değişken tanımlarını bu bölgede topladık.
         /// </summary>
         #region Kontrol Degişkenleri Tanımları
         // Kontroller arasında ve form içerisinde bırakacağımız kenar boşluğu (marjin)
@@ -26,11 +26,25 @@ namespace HesapProje1
         private Button[] dugmeler;
         // Rakam ve işlem düğmeleri için referans değişkenleri
         Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,
-               btnToplama, btnCikarma, btnCarpma, btnBolme, btnSil, btnEsit;
+               btnToplama, btnCikarma, btnCarpma, btnBolme, btnNokta, btnEsit;
+        // İşlem argümanları ve sonuçlarını saklayacak değişkenler
+        private double sayi1, sayi2, sonuc;
+        // Geçerli işlemci karakterleri
+        private char[] islemKarakterleri = new char[] { '+', '-', '*', '/', '=' };
+        // Aktif işlemi gösteren karakter dizgisi
+        // (ki aslında ekran kutusundaki son satırın referansını saklar)
+        private string strIslem;
         #endregion
 
         public HesapForm()
         {
+            // İşlem değerlerini geçersiz sayılar olarak başlat
+            sayi1 = double.NaN;
+            sayi2 = double.NaN;
+            sonuc = double.NaN;
+            // işlem metni başlangıçta boş olsun
+            strIslem = string.Empty;
+            
             InitializeComponent();
 
             FormOlustur();
@@ -73,9 +87,10 @@ namespace HesapProje1
             tbEkran = new TextBox(); // ekranı temsil eden metin kutusu nesnesini oluştur.
             this.Controls.Add(tbEkran); // metin kutusunu formun kontroller koleksiyonuna ekliyoruz.
             tbEkran.BackColor = Color.LightBlue; // ayırt edici bir geriplan rengi belirle.
-            tbEkran.Multiline = true; // ekranı çok satırlı yap ki yüksekliğini biz belirleyelim.
+            tbEkran.Multiline = false; // ekranı şimdilik tek satırlı gibi kullanacağız.
             tbEkran.BorderStyle = BorderStyle.FixedSingle; // ekran kutusu kenarlığı düz çizgi olsun.
-
+            // Ekran kutusuna boş satır ekle ve onu aktif işlem metni olarak kullan
+            
             dugmeler = new Button[16]; // düğmeler dizisini oluştur
             // dizi elemanlarını yeni düğmeler olarak oluştur ve forma ekle
             for(int i= 0; i < 16; i++)
@@ -83,11 +98,13 @@ namespace HesapProje1
                 dugmeler[i] = new Button();
                 dugmeler[i].BackColor = Color.LightBlue;
                 this.Controls.Add(dugmeler[i]);
+                // tüm düğmeler için etiketi aktaran tıklama olay fonksiyonunu ekle
+                dugmeler[i].Click += RakamIslemDugme_Click;
             }
 
             // rakam ve işlem düğme referanslarını belirle
             btn0 = dugmeler[0];
-            btnSil = dugmeler[1];
+            btnNokta = dugmeler[1];
             btnEsit = dugmeler[2];
             btnBolme = dugmeler[3];
             btn1 = dugmeler[4];
@@ -114,12 +131,127 @@ namespace HesapProje1
             btn7.Text = "7";
             btn8.Text = "8";
             btn9.Text = "9";
+      
             btnToplama.Text = "+";
             btnCikarma.Text = "-";
             btnCarpma.Text = "*";
             btnBolme.Text = "/";
-            btnSil.Text = "C";
             btnEsit.Text = "=";
+            // Programın çalıştığı bilgisayarda geçerli olan ayarlara göre
+            // ondalık ayıracı öğren ve ilgili düğmeye etiket olarak  koy.
+            btnNokta.Text = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+
+            // İşlem düğmeleri için tıklama olayını yanıtlayacak fonksiyonlar belirle
+            btnToplama.Click += IslemDugme_Click;
+            btnCikarma.Click += IslemDugme_Click;
+            btnCarpma.Click += IslemDugme_Click;
+            btnBolme.Click += IslemDugme_Click;
+
+            btnEsit.Click += EsitDugme_Click;
+
+            // İşlem düğmelerinin aktiflik durumunu yanıtlayacak fonksiyon belirle
+            btnToplama.EnabledChanged += IslemDugme_EnabledChanged;
+            btnCikarma.EnabledChanged += IslemDugme_EnabledChanged;
+            btnCarpma.EnabledChanged += IslemDugme_EnabledChanged;
+            btnBolme.EnabledChanged += IslemDugme_EnabledChanged;
+        }
+      
+        /// <summary>
+        /// Rakam ya da işlem, tüm düğmeler tıklandığında
+        /// etiket karakteri işlem metni olarak
+        /// ekran kutusuna eklenecektir.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RakamIslemDugme_Click(object sender, EventArgs e)
+        {
+            Button dugme = sender as Button;
+            if (dugme != null)
+            {
+                // Mevcut bir sonuç varsa ekranı sil ve işlem argümanlarını sıfırla
+                if(!double.IsNaN(sonuc))
+                {
+                    sayi1 = double.NaN;
+                    sayi2 = double.NaN;
+                    sonuc = double.NaN;
+                    strIslem = String.Empty;
+                    tbEkran.Text = strIslem;
+                }
+
+                strIslem += dugme.Text;
+                tbEkran.Text = strIslem;
+            }
+        }
+
+        private void IslemDugme_Click(object sender, EventArgs e)
+        {
+            // Bir işlem düğmesi tıklandığında,
+            // yalnızca bir işlem yapılabilsin diye
+            // sonuç hesaplatacak eşittir düğmesi haricindeki
+            // işlem düğmelerini iptal et.
+            btnToplama.Enabled = false;
+            btnCikarma.Enabled = false;
+            btnCarpma.Enabled = false;
+            btnBolme.Enabled = false;
+        }
+
+        private void IslemDugme_EnabledChanged(object sender, EventArgs e)
+        {
+            Button dugme = sender as Button;
+            if (dugme != null)
+            {
+                if(dugme.Enabled)
+                {
+                    dugme.BackColor = Color.LightBlue;
+                    dugme.ForeColor = Color.Black;
+                }
+                else
+                {
+                    dugme.BackColor = Color.LightGray;
+                    dugme.ForeColor = Color.Gray;
+                }
+            }
+        }
+
+        private void EsitDugme_Click(object sender, EventArgs e)
+        {
+            // İşlemci karakterleri ayıraçlar olarak kullanarak 
+            // işlem metnini böl
+            string[] islemArgumanlari = strIslem.Split(islemKarakterleri);
+            sayi1 = Convert.ToDouble(islemArgumanlari[0]);
+            sayi2 = Convert.ToDouble(islemArgumanlari[1]);
+
+            // Hangi işlemci karakteri bulunduysa o işlemi yap ve sonucu hesapla
+            if(strIslem.Contains(islemKarakterleri[0]))
+            {
+                sonuc = sayi1 + sayi2;
+                strIslem += Convert.ToString(sonuc);
+                tbEkran.Text = strIslem;
+            }
+            else if (strIslem.Contains(islemKarakterleri[1]))
+            {
+                sonuc = sayi1 - sayi2;
+                strIslem += Convert.ToString(sonuc);
+                tbEkran.Text = strIslem;
+            }
+            else if (strIslem.Contains(islemKarakterleri[2]))
+            {
+                sonuc = sayi1 * sayi2;
+                strIslem += Convert.ToString(sonuc);
+                tbEkran.Text = strIslem;
+            }
+            else if (strIslem.Contains(islemKarakterleri[3]))
+            {
+                sonuc = sayi1 / sayi2;
+                strIslem += Convert.ToString(sonuc);
+                tbEkran.Text = strIslem;
+            }
+
+            // İşlem düğmelerini tekrar aktifleştir
+            btnToplama.Enabled = true;
+            btnCikarma.Enabled = true;
+            btnCarpma.Enabled = true;
+            btnBolme.Enabled = true;
         }
 
         private void KontrolleriYerlestir()
