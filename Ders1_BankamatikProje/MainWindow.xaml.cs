7dad036723ef20f3cd6e8acdf9c08d303ba88ebc
Hürol Aslan <freebelion@gmail.com>
2022-04-12 19:13:25 +0300

    İlk WPF projesi eklendi.

---
+++ Ders1_BankamatikProje/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ders1_BankamatikProje
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private decimal bakiye;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bakiyeIslem(object sender, EventArgs e)
+        {
+            Button btnIslem = sender as Button;
+            if (btnIslem != null)
+            {
+                bakiye += Convert.ToInt32(btnIslem.Tag);
+                tbBakiye.Text = bakiye.ToString("C");
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbBakiye.Text = bakiye.ToString("C");
+        }
+    }
+}
