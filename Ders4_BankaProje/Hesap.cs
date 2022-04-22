5e7b7023a686a10817856c5432e9ec59cd634557
Hürol Aslan <freebelion@gmail.com>
2022-04-22 19:09:25 +0300

    WPF Ders4 Projesi eklendi

---
+++ Ders4_BankaProje/Hesap.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Ders4_BankaProje
+{
+    public class Hesap : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OzellikDegisti(string ozellik_adi)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(ozellik_adi));
+            }
+        }
+
+        private static int hesapSayisi = 0;
+        private int hesapNo;
+        private decimal hesapBakiyesi = 0;
+
+        public Hesap()
+        {
+            hesapSayisi++;
+            hesapNo = hesapSayisi;
+        }
+
+        public int HesapNo
+        {
+            get { return hesapNo; }
+        }
+
+        public decimal Bakiye
+        {
+            get { return hesapBakiyesi; }
+            private set { hesapBakiyesi = value; }
+        }
+
+        public void ParaEkle(decimal tutar)
+        {
+            Bakiye += tutar;
+            OzellikDegisti("Bakiye");
+        }
+
+        public void ParaCek(decimal tutar)
+        {
+            if(Bakiye >= tutar)
+            {
+                Bakiye -= tutar;
+                OzellikDegisti("Bakiye");
+            }
+        }
+    }
+}
---
+++ Ders4_BankaProje/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ders4_BankaProje
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Hesap> Hesaplar { get; private set; }
+        public Hesap AktifHesap { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = this;
+            Hesaplar = new ObservableCollection<Hesap>(); 
+            
+            for(int i=0; i < 5; i++)
+            {
+                Hesaplar.Add(new Hesap());
+            }
+        }
+
+        private void bakiyeIslem(object sender, EventArgs e)
+        {
+            Button btnIslem = sender as Button;
+            if (btnIslem != null)
+            {
+                decimal tutar = Convert.ToDecimal(btnIslem.Tag);
+                if(tutar > 0)
+                {
+                    AktifHesap.ParaEkle(tutar);
+                }
+                else
+                {
+                    AktifHesap.ParaCek(Math.Abs(tutar));
+                }
+            }
+        }
+    }
+}
