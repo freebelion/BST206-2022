c466b50ea1408f722452b69609b61c10573bed47
Hürol Aslan <freebelion@gmail.com>
2022-04-30 21:15:18 +0300

    WPF "OrnekProje" eklendi

---
+++ OrnekProje/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrnekProje
+{
+    public class Market : ObservableCollection<Category> { }
+    public class Category
+    {
+        public Guid ID { get; private set; }
+        public string Name { get; set; }
+        public ObservableCollection<Product> Products { get; set; }
+
+        public Category()
+        {
+            this.ID = Guid.NewGuid();
+            this.Name = "Category_" + this.ID.ToString().Split('-')[0];
+            Products = new ObservableCollection<Product>();
+        }
+    }
+}
---
+++ OrnekProje/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace OrnekProje
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddCategory(object sender, RoutedEventArgs e)
+        {
+            OrnekMarket.Add(new Category());
+        }
+
+        private void AddProduct(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if(btn != null)
+            {
+                Category reyon = (Category)btn.DataContext;
+                reyon.Products.Add(new Product());
+            }
+        }
+
+        private void SetImage(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                Product prod = (Product)btn.DataContext;
+                OpenFileDialog dlgDosyaAc = new OpenFileDialog();
+                dlgDosyaAc.Multiselect = false;
+                dlgDosyaAc.Filter = "Resim dosyaları (*.bmp, *.jpg, *png)|*.bmp;*.jpg;*.png";
+                if (dlgDosyaAc.ShowDialog() == true)
+                {
+                    prod.ImageFile = dlgDosyaAc.FileName;
+                    btn.DataContext = prod;
+                }
+            }
+        }
+    }
+}
